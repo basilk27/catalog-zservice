f80b613e82ec605a19431cd99dcec654e429e5ba
basilk27 <basilkiwanuka@icloud.com>
2024-03-27 19:49:12 -0400

    changes to stage file

--- src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
+++ src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.mbsystems.catalogservice.controller;
 
+import com.mbsystems.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final PolarProperties polarProperties;
 
     @GetMapping("/")
     public String getGreetings() {
-        return "Welcome to the book catalog";
+        return this.polarProperties.getGreeting();
     }
 }
